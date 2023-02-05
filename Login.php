8dcdcda4bc9c563f7040158054ef75848a77f107
FernandoGalo <93693934+FernandoGalo@users.noreply.github.com>
2023-02-04 21:48:12 -0600

    Add files via upload

--- Login.php
+++ Login.php
@@ -3,22 +3,40 @@
 <head>
     <title> Login </title>
     <link rel="stylesheet" href="css/normalize.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:wght@400;700&family=Staatliches&display=swap" rel="stylesheet">
+    <!-- Preload -->
+    <link rel="preload" href="css/normalize.css">
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <link rel="preload" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<div class="logo">
+                <img src="img/asociacion.jpg" alt=""> 
+            </div>
     <form actions2="controlador_login.php" method="post">
+     <?php
+     include ("conexion_BD.php");
+     include ("controlador_login.php");
+     ?>
             <h2>Usuario</h2>
             <?php if(isset($_GET['error'])) { ?>
              <p class="error"><?php echo $_GET['error']; ?></p>
             <?php } ?>
         <label>Usuario</label>
-        <input type="usuario" name="Usuario" placeholder="ingrese su usuario"><br>
+        <input type="text" name="usuario" placeholder="ingrese su usuario"><br>
         <label>contraseña</label>
-        <input type="contraseña" name="Contra" placeholder="ingrese su contraseña"><br>
-
-        <button type="submit">Login</button>
-
+        <input type="password" name="contra" placeholder="ingrese su contraseña"><br>
+        <input type="submit" Class="btn" name="btn_Login" value="Iniciar secion" ></br>
+       
         <a href="renovar-Contra.html">¿olvidaste la contraseña?</a>
     </form>
+    <li> <a href="index.html">Home</a></li>
 </body>
 
 </html>
--- controlador_login.php
+++ controlador_login.php
@@ -1,13 +1,15 @@
 <?php
-if (!empty($_POST["BTNIngresar_Login"])) {
-    if (empty($_POST["Usuario_Login"] and empty($_POST["Contraseña_login"]))) {
+if (!empty($_POST["btn_Login"])) {
+    if (empty($_POST["usuario"]) and empty($_POST["contra"])) 
+    {
         echo '<div class="alert alert-danger">los campos estan vacios</div>';
-    } else {
-        $usuario = $_POST["Usuario_Login"];
-        $contraseña = $_POST["Contraseña_login"];
-        $sql = $conexion->query("select * from Login where Usuario='$usuario' and contraseña='$contraseña'");
+    } else{
+        $usuario=$_POST["usuario"];
+        $contra=$_POST["contra"];
+        $sql=$conexion->query("SELECT * FROM Login where nombre='$usuario' and contraseña='$contra'  ");
         if ($datos=$sql->fetch_object()) {
-            header("sistema_principal.php");
+            header("location: Sistema_Principal.php");
+            echo "Ingreso al sistema exitoso";
         } else {
             echo '<div class="alert alert-danger">Usuario o contraseña incorrectos </div>';
         }
