8db29e188f97f8396c2a1dbb3491e9d0ad397a30
ElidaAlvarado <elida.alvarado@unah.hn>
2023-04-01 20:36:24 -0600

    Tabla Parametros

--- Sistema/seguridad/ParametrosAdm.php
+++ Sistema/seguridad/ParametrosAdm.php
@@ -4,6 +4,7 @@ require '../../conexion_BD.php';
 require_once "../../EVENT_BITACORA.php";
 session_start();     
 $usuario=$_SESSION['usuario'];
+$ID_Rol=$_SESSION['ID_Rol'];
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -21,8 +22,11 @@ $usuario=$_SESSION['usuario'];
 
 <?php include '../sidebar.php'; ?>
 
-		<!-- Pagina de contenido-->
-    <section class="full-box dashboard-contentPage" style="overflow-y: auto;">
+	<!--Seccion donde va toda la barra lateral -->
+	<?php include '../sidebar.php'; ?>
+
+	<!-- Pagina de contenido-->
+	<section class="full-box dashboard-contentPage" style="overflow-y: auto;">
 		<!-- Barra superior -->
 		<nav class="full-box dashboard-Navbar">
 			<ul class="full-box list-unstyled text-right">
@@ -33,9 +37,68 @@ $usuario=$_SESSION['usuario'];
 		</nav>
 		<!-- Muestra el contenido de la pagina -->
 		<div class="container-fluid">
-			<h1>Aqui irian los parametros</h1>
-		</div>
-		<div class="container-fluid">
+        <div class="row">
+              <div class="col-md-12">
+                  <div class="box">
+                    <div class="box-header with-border">
+                          <h1 class="box-title">Mantenimiento Parametros</h1>
+                        </div>
+                        <br>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- centro -->
+                    <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_consultar=1 and ID_Rol=$ID_Rol ");
+if ($datos=$sql->fetch_object()) { ?>
+                    <div class="panel-body table-responsive" id="listadoregistros">
+                        <table style="text-align:center" id="tbllistado" class="table table-bordered table-hover">
+                        
+                        <!-- Buscar -->
+                        <form action="" method="post">
+                            <label for="campo">Buscar:</label>
+                            <input type="text" id="buscador" onkeyup="buscarTabla()" placeholder="Buscar...">
+                        </form>
+                        <thead>
+                            <th style="text-align:center">ID</th>
+                            <th style="text-align:center">Paramero</th>
+                            <th style="text-align:center">Valor</th>
+							<th style="text-align:center">Acciones</th>
+                        </thead>
+                          <tbody>                            
+                          </tbody>
+                          <tfoot>
+
+                          <?php
+                          $sql="SELECT * from tbl_ms_parametros";
+                          $result=mysqli_query($conexion,$sql);
+
+                           while($mostrar=mysqli_fetch_array($result)){
+                           ?>
+
+                            <tr>
+                              <td><?php echo $mostrar['ID_Parametro']?></td> 
+                              <td><?php echo $mostrar['Parametro']?></td> 
+                              <td><?php echo $mostrar['Valor']?></td>
+        
+                              <td>
+                              <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Actualizacion=1 and ID_Rol=$ID_Rol");
+									if ($datos=$sql->fetch_object()) { ?>
+                              <a href='Update_Parametros.php?ID_Parametro=<?php echo $mostrar['ID_Parametro']; ?>' class='boton-editar'>
+                              <i class='zmdi zmdi-edit'></i> Editar
+                              <?php } ?>
+                              </a>
+                            </td>
+                             </tr>
+                            <?php
+                             }
+                             ?>     
+                          </tfoot>
+                        </table>
+                    </div>
+                    <?php } ?>
+                    <!--Fin centro -->
+                  </div><!-- /.box -->
+              </div><!-- /.col -->
+          </div><!-- /.row -->
 		</div>
 	</section>
 
---
+++ Sistema/seguridad/Update_Parametros.php
@@ -0,0 +1,252 @@
+<?php
+    include("../../conexion_BD.php");
+    require_once "../../EVENT_BITACORA.php";
+    
+    session_start();     
+$usuario=$_SESSION['usuario'];
+$ID_Rol=$_SESSION['ID_Rol'];
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+<title>Inicio</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="../../css/main.css">
+
+</head>
+<body>
+	<!--Seccion donde va toda la barra lateral -->
+	<section class="full-box cover dashboard-sideBar">
+		<div class="full-box dashboard-sideBar-bg btn-menu-dashboard"></div>
+		<div class="full-box dashboard-sideBar-ct">
+			<!--Muestra el titulo de la barra lateral-->
+			<div class="full-box text-uppercase text-center text-titles dashboard-sideBar-title">
+				Creo en ti <i class="zmdi zmdi-close btn-menu-dashboard visible-xs"></i>
+			</div>
+			<!-- Informacion de usuario de la barra lateral -->
+			<div class="full-box dashboard-sideBar-UserInfo">
+				<figure class="full-box">
+					<img src="./img/avatar.jpg" alt="UserIcon">
+					<figcaption class="text-center text-titles"><?php echo $usuario; ?></figcaption>
+				</figure>
+				<ul class="full-box list-unstyled text-center">
+					<li>
+						<a href="#!">
+							<i class="zmdi zmdi-settings"></i>
+						</a>
+					</li>
+					<li>
+						<a href="./Pantallas/Login.php" class="btn-exit-system">
+							<i class="zmdi zmdi-power"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- Menu de la barra lateral -->
+			<ul class="list-unstyled full-box dashboard-sideBar-Menu">
+				<li>
+					<a href="#!" class="btn-sideBar-SubMenu">
+						<i class="zmdi zmdi-account-add zmdi-hc-fw"></i> Usuarios <i class="zmdi zmdi-caret-down pull-right"></i>
+					</a>
+					<ul class="list-unstyled full-box">
+						<li>
+							<a href="usuariosAdm.php"><i class="zmdi zmdi-account zmdi-hc-fw"></i> Mantenimiento usuarios</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#!" class="btn-sideBar-SubMenu">
+						<i class="zmdi zmdi-shield-security zmdi-hc-fw"></i> Seguridad <i class="zmdi zmdi-caret-down pull-right"></i>
+					</a>
+					<ul class="list-unstyled full-box">
+						<li>
+							<a href="./Controladores/Bitacora.php"><i class="zmdi zmdi-file zmdi-hc-fw"></i> Bitacora </a>
+						</li>
+					</ul>
+          <li>
+					<a href="#!" class="btn-sideBar-SubMenu">
+						<i class="zmdi zmdi-shield-security zmdi-hc-fw"></i> Proyectos <i class="zmdi zmdi-caret-down pull-right"></i>
+					</a>
+					<ul class="list-unstyled full-box">
+						<li>
+							<a href=""><i class="zmdi zmdi-file zmdi-hc-fw"></i> Mantenimiento Proyectos </a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#!" class="btn-sideBar-SubMenu">
+						<i class="zmdi zmdi-money-box"></i> Fondos <i class="zmdi zmdi-caret-down pull-right"></i>
+					</a>
+					<ul class="list-unstyled full-box">
+						<li>
+							<a href="FondosAdm.php"><i class="zmdi zmdi-file zmdi-hc-fw"></i> Mantenimiento Fondos </a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#!" class="btn-sideBar-SubMenu">
+						<i class="zmdi zmdi-accounts"></i> Voluntarios <i class="zmdi zmdi-caret-down pull-right"></i>
+					</a>
+					<ul class="list-unstyled full-box">
+						<li>
+							<a href="Voluntarios.php"><i class="zmdi zmdi-file zmdi-hc-fw"></i> Mantenimiento voluntarios </a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#!" class="btn-sideBar-SubMenu">
+						<i class="zmdi zmdi-money"></i> Pagos <i class="zmdi zmdi-caret-down pull-right"></i>
+					</a>
+					<ul class="list-unstyled full-box">
+						<li>
+							<a href=""><i class="zmdi zmdi-file zmdi-hc-fw"></i> Mantenimiento pagos </a>
+						</li>
+					</ul>
+				</li>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+
+    <?php
+        if(isset($_POST['Enviar_P'])){        
+            //aqui entra sio el usuario ha presionado el boton enviar
+            $ID_Parametro=$_POST['ID_Parametro'];
+            $Parametro=$_POST['Parametro'];
+            $Valor=$_POST['Valor'];
+            $Fecha_actual = date('Y-m-d');
+
+            //si lo que esta en el form esta vacio
+            if(empty($Valor)){
+                echo"<p class='error'>* Debe colocar un valor</p>";
+            }
+
+
+
+
+
+            //UPDATE tbl_voluntarios SET Usuario=$user WHERE Nombre_Usuario=$id;
+            $sql="UPDATE tbl_ms_parametros SET Valor = '$Valor', ID_Usuario='$ID_Rol', Fecha_Modificacion='$Fecha_actual' WHERE ID_Parametro='$ID_Parametro';";
+            $resultado=mysqli_query($conexion,$sql);
+
+            if($resultado){
+                echo "<script language='JavaScript'>
+                        alert('Los datos se actualizaron correctamente');
+						location.assign('ParametrosAdm.php');
+						</script>";
+                    
+                   
+                    require_once "../../EVENT_BITACORA.php";
+                            $model = new EVENT_BITACORA;
+                            session_start();
+                            $_SESSION['VOLBitacoraUP']=$Nombre_Voluntario;
+
+                            $model->RegUptVol();
+            }else{
+                echo "<script language='JavaScript'>
+                alert('Los datos NO se actualizaron');
+            location.assign('ParametrosAdm.php');
+            </script>";
+            }
+            mysqli_close($conexion);
+        }
+        else{
+            //si el usuario NO ha presionado el boton enviar
+            $id=$_GET['ID_Parametro']; //recuperar el id que se envia desde el home.html
+            $sql2="SELECT * FROM tbl_ms_parametros where ID_Parametro='".$id."'";
+            $resultado=mysqli_query($conexion,$sql2);
+
+            $fila=mysqli_fetch_assoc($resultado);
+
+            $ID_Parametro=$fila['ID_Parametro'];
+            $Parametro=$fila['Parametro'];
+            $Valor=$fila['Valor'];//recuperando los datos desde la BD
+           
+            mysqli_close($conexion);
+
+    ?>
+    	<!-- Pagina de contenido-->
+	<section class="full-box dashboard-contentPage" style="overflow-y: auto;">
+		<!-- Barra superior -->
+		<nav class="full-box dashboard-Navbar">
+			<ul class="full-box list-unstyled text-right">
+				<li class="pull-left">
+					<a href="#!" class="btn-menu-dashboard"><i class="zmdi zmdi-more-vert"></i></a>
+				</li>
+			</ul>
+		</nav>
+		<!-- Muestra el contenido de la pagina -->
+		<div class="container-fluid">
+        <div class="row">
+              <div class="col-md-12">
+                  <div class="box">
+                    <div class="box-header with-border">
+                          <h1 class="box-title">Editar Parametro</h1>
+                        </div>
+                        <br>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- centro -->
+                    <div class="panel-body">
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
+                        <div class="container">
+                          <div class="row">
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <label>ID Parametros (*):</label>
+                            <input type="hidden" name="ID_Parametro" id="ID_Parametro">
+                            <input type="text" class="form-control" name="ID_Parametro" id="ID_Parametro" maxlength="10" value="<?php echo $ID_Parametro; ?>" readonly>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                            <label>Parametros (*):</label>
+                            <input type="hidden" name="Parametro" id="Parametro">
+                            <input style="text-transform:uppercase" type="text" class="form-control" name="Parametro" id="Parametro" maxlength="30" value="<?php echo $Parametro; ?>" readonly>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                            <label>Valor (*):</label>
+                            <input type="hidden" name="Valor" id="Valor">
+                            <input type="text" class="form-control" name="Valor" id="Valor" maxlength="40" placeholder="Ingrese el valor" value="<?php echo $Valor; ?>" required>
+                          </div>
+                          <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                          <button class="btn btn-primary" type="submit" name="Enviar_P" value="AGREGAR"><i class="zmdi zmdi-download"></i> Guardar</button>
+                          <button class="btn btn-danger" type="button">
+                          <a href="ParametrosAdm.php" style="color:white; text-decoration:none;">
+                          <i class="zmdi zmdi-close-circle"></i> Cancelar
+                          </a>
+                          </button>
+                          </div>
+                          </div>
+                          </div>
+                        </form>
+                        </div>
+                    <!--Fin centro -->
+                  </div><!-- /.box -->
+              </div><!-- /.col -->
+          </div><!-- /.row -->
+		</div>
+	</section>
+                        
+
+    <?php
+        }
+    ?>
+
+
+
+	<!--script en java para los efectos-->
+  <script src="../../js/jquery-3.1.1.min.js"></script>
+  <script src="../../js/events.js"></script>
+	<script src="../../js/main.js"></script>
+  <script src="../../js/usuario.js"></script>
+
+
+</body>
+</html>
