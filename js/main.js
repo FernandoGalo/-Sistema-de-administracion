1887a20d75fab087fb3f6fa85e884dd7e32ccc49
unknown <joseph.centeno@unah.hn>
2023-04-12 09:35:25 -0600

    Libreria alerta

--- js/main.js
+++ js/main.js
@@ -10,6 +10,20 @@ $(document).ready(function(){
 			SubMenu.addClass('show-sideBar-SubMenu');
 		}
 	});
+	$('.btn-exit-system').on('click', function(){
+		swal({
+		  	title:'¿Estas seguro?',
+		  	text: "La sesión actual se cerrará",
+		  	type: 'warning',
+		  	showCancelButton: true,
+		  	confirmButtonColor: '#03A9F4',
+		  	cancelButtonColor: '#F44336',
+		  	confirmButtonText: '<i class="zmdi zmdi-run"></i> Si, Salir!',
+		  	cancelButtonText: '<i class="zmdi zmdi-close-circle"></i> No, Cancelar!'
+		}).then(function () {
+			window.location.href="../../Pantallas/Login.php";
+		});
+	});
 	$('.btn-menu-dashboard').on('click', function(){
 		var body=$('.dashboard-contentPage');
 		var sidebar=$('.dashboard-sideBar');
@@ -21,4 +35,43 @@ $(document).ready(function(){
 			sidebar.addClass('hide-sidebar').removeClass('show-sidebar');
 		}
 	});
+	$('.btn-search').on('click', function(){
+		swal({
+		  title: 'What are you looking for?',
+		  confirmButtonText: '<i class="zmdi zmdi-search"></i>  Search',
+		  confirmButtonColor: '#03A9F4',
+		  showCancelButton: true,
+		  cancelButtonColor: '#F44336',
+		  cancelButtonText: '<i class="zmdi zmdi-close-circle"></i> Cancel',
+		  html: '<div class="form-group label-floating">'+
+			  		'<label class="control-label" for="InputSearch">write here</label>'+
+			  		'<input class="form-control" id="InputSearch" type="text">'+
+				'</div>'
+		}).then(function () {
+		  swal(
+		    'You wrote',
+		    ''+$('#InputSearch').val()+'',
+		    'success'
+		  )
+		});
+	});
+	$('.btn-modal-help').on('click', function(){
+		$('#Dialog-Help').modal('show');
+	});
 });
+(function($){
+    $(window).on("load",function(){
+        $(".dashboard-sideBar-ct").mCustomScrollbar({
+        	theme:"light-thin",
+        	scrollbarPosition: "inside",
+        	autoHideScrollbar: true,
+        	scrollButtons: {enable: true}
+        });
+        $(".dashboard-contentPage, .Notifications-body").mCustomScrollbar({
+        	theme:"dark-thin",
+        	scrollbarPosition: "inside",
+        	autoHideScrollbar: true,
+        	scrollButtons: {enable: true}
+        });
+    });
+})(jQuery);
