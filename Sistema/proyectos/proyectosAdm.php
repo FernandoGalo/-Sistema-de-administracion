4361ae3994045b655c61ff43001e6cd125058370
FernandoGalo <fernando.galo@unah.hn>
2023-04-18 00:40:54 -0600

    proyectos tabla agregada

--- Sistema/proyectos/proyectosAdm.php
+++ Sistema/proyectos/proyectosAdm.php
@@ -13,6 +13,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="../../css/main.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script type="text/javascript">
     function confirmar(){
       return confirm('¿Está Seguro?, se eliminará el proyecto');
@@ -38,70 +39,174 @@
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                          <h1 class="box-title">Mantenimiento Proyectos</h1>
-                          <button class="btn btn-success" id="btnagregar" name="btnAgregar" onclick="mostrarform(true)"><i class="zmdi zmdi-account-add"></i>Agregar Usuario</button>
+                          <h1 class="box-title">Gestion Proyectos</h1>
+                          <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Insercion=1 and ID_Rol=$ID_Rol and ID_Objeto=6");
+if ($datos=$sql->fetch_object()) { ?>
+                          <button class="btn btn-success" id="btnagregar" name="btnAgregar" onclick="mostrarform(true)"><i class="zmdi zmdi-account-add"></i>Agregar Proyecto</button>
                           <div class="box-tools pull-right">
+                            <?php } ?>
                         </div>
                         <br>
                     </div>
                     <!-- /.box-header -->
                     <!-- centro -->
-                    <div class="panel-body table-responsive" id="listadoregistros">
-                        <table id="tbllistado" class="table table-bordered table-hover">
-                        
-                        <!-- Buscar -->
-                        <form action="" method="post">
-                            <label for="campo">Buscar:</label>
-                            <input type="text" id="buscador" onkeyup="buscarTabla()" placeholder="Buscar...">
-                        </form>
+                    <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_consultar=1 and ID_Rol=$ID_Rol and ID_Objeto=6");
+if ($datos=$sql->fetch_object()) { ?>
+<div class="panel-body" id="listadoregistros">
+<main>
+        <div class="container py-4 text-center">
+
+            <div class="row g-4">
+
+                <div class="col-auto">
+                    <label for="num_registros" class="col-form-label">Mostrar: </label>
+                </div>
+
+                <div class="col-auto">
+                    <select name="num_registros" id="num_registros" class="form-select">
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+                </div>
+
+                <div class="col-auto">
+                    <label for="num_registros" class="col-form-label">registros </label>
+                </div>
+
+                <div class="col-5"></div>
+
+                <div class="col-auto">
+                    <label for="campo" class="col-form-label">Buscar: </label>
+                </div>
+                <div class="col-auto">
+                    <input type="text" name="campo" id="campo" class="form-control">
+                </div>
+            </div>
+
+            <div class="row py-4">
+                <div class="col">
+                    <table class="table table-sm table-bordered table-striped">
                         <thead>
-                            <th>ID</th>
-                            <th>Nombre del proyecto</th>
-                            <th>Fecha de inicio</th>
-                            <th>Fecha final</th>
-                            <th>Fondos proyecto</th>
-                            <th>Estado Proyecto</th>
-                            <th>Acciones</th>
-                          </thead>
-                          <tbody>                            
-                          </tbody>
+                            <th class="sort asc">ID</th>
+                            <th class="sort asc">Proyecto</th>
+                            <th class="sort asc">Fecha de Inicio</th>
+                            <th class="sort asc">Fecha de finalización</th>
+                            <th class="sort asc">Fondos proyecto</th>
+                            <th class="sort asc">Estado Proyecto</th>
 
-                          <tfoot>
-                          <?php
-                          $sql="SELECT * from tbl_proyectos";
-                          $result=mysqli_query($conexion,$sql);
+                            <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Actualizacion=1 and ID_Rol=$ID_Rol and ID_Objeto=6");
+if ($datos=$sql->fetch_object()) {?>
+                            <th></th>
+                            <?php } ?>
+                            <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Eliminacion=1 and ID_Rol=$ID_Rol and ID_Objeto=6");
+if ($datos=$sql->fetch_object()) { ?>
+                            <th></th>
+                            <?php } ?>
+                            <th></th>
+                        </thead>
+                        <!-- El id del cuerpo de la tabla. -->
+                        <tbody id="content">
 
-                           while($mostrar=mysqli_fetch_array($result)){
-                           ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
-                            <tr>
-                              <td><?php echo $mostrar['ID_proyecto']?></td> 
-                              <td><?php echo $mostrar['Nombre_del_proyecto']?></td> 
-                              <td><?php echo $mostrar['Fecha_de_inicio_P']?></td>
-                              <td><?php echo $mostrar['Fecha_final_P']?></td>
-                              <td><?php echo $mostrar['Fondos_proyecto']?></td>
-                              <td><?php echo $mostrar['Estado_Proyecto']?></td>
-                              <td>
-                              <a href='Update_Donan.php?ID_proyecto=<?php echo $mostrar['ID_proyecto']; ?>' class='boton-editar'>
-                              <i class='zmdi zmdi-edit'></i>
-                              </a>
-                              <a href='Delete_Donan.php?ID_proyecto=<?php echo $mostrar['ID_proyecto']; ?>' onclick='return confirmar()' class='boton-eliminar'>
-                              <i class='zmdi zmdi-delete'></i>
-                              </a>
-                              <a href='ProyectosGest.php?ID_proyecto=<?php echo $mostrar['ID_proyecto']; ?>' class='boton-ver'>
-                              <i class='zmdi zmdi-eye'></i>
-                              </a>
-                            </td>
-                             </tr>
-                            <?php
-                             }
-                             ?> 
+            <div class="row">
+                <div class="col-6">
+                    <label id="lbl-total"></label>
+                </div>
+
+                <div class="col-6" id="nav-paginacion"></div>
+
+                <input type="hidden" id="pagina" value="1">
+                <input type="hidden" id="orderCol" value="0">
+                <input type="hidden" id="orderType" value="asc">
+            </div>
+        </div>
+    </main>
+</div>
+    <script>
+        /* Llamando a la función getData() */
+        getData()
+
+        /* Escuchar un evento keyup en el campo de entrada y luego llamar a la función getData. */
+        document.getElementById("campo").addEventListener("keyup", function() {
+            getData()
+        }, false)
+        document.getElementById("num_registros").addEventListener("change", function() {
+            getData()
+        }, false)
 
 
+        /* Peticion AJAX */
+        function getData() {
+            let input = document.getElementById("campo").value
+            let num_registros = document.getElementById("num_registros").value
+            let content = document.getElementById("content")
+            let pagina = document.getElementById("pagina").value
+            let orderCol = document.getElementById("orderCol").value
+            let orderType = document.getElementById("orderType").value
 
-                          </tfoot>
-                        </table>
-                    </div>
+            if (pagina == null) {
+                pagina = 1
+            }
+            let url = "Gestion_tbl_proyectos.php"
+            let formaData = new FormData()
+            formaData.append('campo', input)
+            formaData.append('registros', num_registros)
+            formaData.append('pagina', pagina)
+            formaData.append('orderCol', orderCol)
+            formaData.append('orderType', orderType)
+
+            fetch(url, {
+                    method: "POST",
+                    body: formaData
+                }).then(response => response.json())
+                .then(data => {
+                    content.innerHTML = data.data
+                    document.getElementById("lbl-total").innerHTML = 'Mostrando ' + data.totalFiltro +
+                        ' de ' + data.totalRegistros + ' registros'
+                    document.getElementById("nav-paginacion").innerHTML = data.paginacion
+                }).catch(err => console.log(err))
+        }
+
+        function nextPage(pagina){
+            document.getElementById('pagina').value = pagina
+            getData()
+        }
+
+        let columns = document.getElementsByClassName("sort")
+        let tamanio = columns.length
+        for(let i = 0; i < tamanio; i++){
+            columns[i].addEventListener("click", ordenar)
+        }
+
+        function ordenar(e){
+            let elemento = e.target
+
+            document.getElementById('orderCol').value = elemento.cellIndex
+
+            if(elemento.classList.contains("asc")){
+                document.getElementById("orderType").value = "asc"
+                elemento.classList.remove("asc")
+                elemento.classList.add("desc")
+            } else {
+                document.getElementById("orderType").value = "desc"
+                elemento.classList.remove("desc")
+                elemento.classList.add("asc")
+            }
+
+            getData()
+        }
+
+    </script>
+
+    <!-- Bootstrap core JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<?php } ?>
                     <div class="panel-body" id="formularioregistros">
                         <form name="formulario" id="formulario" action="Insert_Usuarios.php" method="POST">
                         <div class="container">
