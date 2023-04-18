f703483e8a7be2e2ba8708aa277e2220126b638b
FernandoGalo <fernando.galo@unah.hn>
2023-04-18 01:17:44 -0600

    Fondos tabla lista

--- Sistema/Fondos/FondosAdm.php
+++ Sistema/Fondos/FondosAdm.php
@@ -39,34 +39,12 @@ $R_F_Vencida= date("Y-m-j",strtotime($R_Fecha_actual."+ ".$diasV." days")); /*le
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="../../css/main.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script type="text/javascript">
     function confirmar(){
       return confirm('¿Está Seguro?, se eliminará el Fondo');
     }
   </script>
-<script>
-function redirigirProyectos() {
-  var url = "../Proyectos/ProyectosAdm.php";
-  if (url) {
-    window.location.href = url;
-  }
-}
-
-function redirigirDonaciones() {
-  var url = "DonacAdm.php";
-  if (url) {
-    window.location.href = url;
-  }
-}
-
-
-function redirigirTFondos() {
-  var url = "Tipo_Fondo_Adm.php";
-  if (url) {
-    window.location.href = url;
-  }
-}
-</script>
 </head>
 <body>
 	<!--Seccion donde va toda la barra lateral -->
@@ -88,14 +66,11 @@ function redirigirTFondos() {
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                          <h1 class="box-title">Mantenimiento de fondos</h1>
+                          <h1 class="box-title">Mantenimiento de Fondos</h1>
                           <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Insercion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
 if ($datos=$sql->fetch_object()) { ?>
 
                           <button class="btn btn-success" id="btnagregar" name="btnAgregar" onclick="mostrarform(true)"><i class="zmdi zmdi-account-add"></i> Agregar Tipo de Fondo</button>
-                          <button id="proyectos-btn" onclick="redirigirProyectos()">Ir a Proyectos </button>
-<button id="donaciones-btn" onclick="redirigirDonaciones()">Ir a Donaciones</button>
-<button id="T_Fondos-btn" onclick="redirigirTFondos()">Ir a Tipos de Fondos</button>
                           <div class="box-tools pull-right">
                             <?php } ?>
                         </div>
@@ -105,72 +80,163 @@ if ($datos=$sql->fetch_object()) { ?>
                     <!-- centro -->
                     <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_consultar=1 and ID_Rol=$ID_Rol and ID_Objeto=7 ");
 if ($datos=$sql->fetch_object()) { ?>
-                    <div class="panel-body table-responsive" id="listadoregistros">
-                        <table id="tbllistado" class="table table-bordered table-hover">
-                        
-                        <!-- Buscar -->
-                        <form action="" method="post">
-                            <label for="campo">Buscar:</label>
-                            <input type="text" id="buscador" onkeyup="buscarTabla()" placeholder="Buscar...">
-                        </form>
+<div class="panel-body" id="listadoregistros">
+<main>
+        <div class="container py-4 text-center">
+
+            <div class="row g-4">
+
+                <div class="col-auto">
+                    <label for="num_registros" class="col-form-label">Mostrar: </label>
+                </div>
+
+                <div class="col-auto">
+                    <select name="num_registros" id="num_registros" class="form-select">
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+                </div>
+
+                <div class="col-auto">
+                    <label for="num_registros" class="col-form-label">registros </label>
+                </div>
+
+                <div class="col-5"></div>
+
+                <div class="col-auto">
+                    <label for="campo" class="col-form-label">Buscar: </label>
+                </div>
+                <div class="col-auto">
+                    <input type="text" name="campo" id="campo" class="form-control">
+                </div>
+            </div>
+
+            <div class="row py-4">
+                <div class="col">
+                    <table class="table table-sm table-bordered table-striped">
                         <thead>
-                            <th>ID fondo</th>
-                            <th>Tipo de fondo</th>
-                            <th>Nombre_del_Objeto</th>
-                            <th>Cantidad_Recibida</th>
-                            <th>valor monetario</th>
-                            <th>Proyecto</th>
-                            <th>Donante </th>
-                            <th>Fecha de aquisicion </th>
-                            <th>acciones </th>
-                          </thead>
-                          <tbody>                            
-                          </tbody>
-                          <tfoot>
+                            <th class="sort asc">ID</th>
+                            <th class="sort asc">Tipo de Fondo</th>
+                            <th class="sort asc">Nombre del Objeto</th>
+                            <th class="sort asc">Cantidad recibida</th>
+                            <th class="sort asc">Valor_monetario</th>
+                            <th class="sort asc">Proyecto</th>
+                            <th class="sort asc">Donante</th>
+                            <th class="sort asc">Fecha de Adquisicion</th>
 
-                          <?php
-                          $sql="SELECT f.ID_de_fondo,tf.nombre_T_Fondo,f.Nombre_del_Objeto,f.Cantidad_Rec,f.Valor_monetario, p.Nombre_del_proyecto, d.Nombre_D, f.Fecha_de_adquisicion_F
-                          FROM tbl_fondos f 
-                          JOIN tbl_tipos_de_fondos tf ON f.ID_Tipo_Fondo = tf.ID_Tipo_Fondo
-                          JOIN tbl_donantes d ON f.ID_Donante = d.ID_Donante
-                          JOIN tbl_proyectos p ON f.ID_Proyecto = p.ID_proyecto 
-                          where f.ID_proyecto=$IDProyecto";
-                          $result=mysqli_query($conexion,$sql);
-
-                           while($mostrar=mysqli_fetch_array($result)){
-                           ?>
-
-                            <tr>
-                              <td><?php echo $mostrar['ID_de_fondo']?></td> 
-                              <td><?php echo $mostrar['nombre_T_Fondo']?></td>
-                              <td><?php echo $mostrar['Nombre_del_Objeto']?></td>
-                              <td><?php echo $mostrar['Cantidad_Rec']?></td>
-                              <td><?php echo $mostrar['Valor_monetario']?></td>
-                              
-                              <td><?php echo $mostrar['Nombre_del_proyecto']?></td>
-                              <td><?php echo $mostrar['Nombre_D']?></td> 
-                              <td><?php echo $mostrar['Fecha_de_adquisicion_F']?></td>
-                              <td>
-                              <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Actualizacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
+                            <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Actualizacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
 if ($datos=$sql->fetch_object()) { ?>
-                              <a href='Update_Fondo.php?ID_de_fondo=<?php echo $mostrar['ID_de_fondo']; ?>' class='boton-editar'>
-                              <i class='zmdi zmdi-edit'></i>
-                                <?php } ?>
-                              </a>
-                              <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Eliminacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
+                            <th></th>
+                            <?php } ?>
+                            <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Eliminacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
 if ($datos=$sql->fetch_object()) { ?>
-                              <a href='Delete_Fondo.php?ID_de_fondo=<?php echo $mostrar['ID_de_fondo']; ?>' onclick='return confirmar()' class='boton-eliminar'>
-                              <i class='zmdi zmdi-delete'></i>
-                              <?php } ?>
-                              </a>
-                            </td>
-                             </tr>
-                            <?php
-                             }
-                             ?>     
-                          </tfoot>
-                        </table>
-                    </div>
+                            <th></th>
+                            <?php } ?>
+                        </thead>
+                        <!-- El id del cuerpo de la tabla. -->
+                        <tbody id="content">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-6">
+                    <label id="lbl-total"></label>
+                </div>
+
+                <div class="col-6" id="nav-paginacion"></div>
+
+                <input type="hidden" id="pagina" value="1">
+                <input type="hidden" id="orderCol" value="0">
+                <input type="hidden" id="orderType" value="asc">
+            </div>
+        </div>
+    </main>
+</div>
+    <script>
+        /* Llamando a la función getData() */
+        getData()
+
+        /* Escuchar un evento keyup en el campo de entrada y luego llamar a la función getData. */
+        document.getElementById("campo").addEventListener("keyup", function() {
+            getData()
+        }, false)
+        document.getElementById("num_registros").addEventListener("change", function() {
+            getData()
+        }, false)
+
+
+        /* Peticion AJAX */
+        function getData() {
+            let input = document.getElementById("campo").value
+            let num_registros = document.getElementById("num_registros").value
+            let content = document.getElementById("content")
+            let pagina = document.getElementById("pagina").value
+            let orderCol = document.getElementById("orderCol").value
+            let orderType = document.getElementById("orderType").value
+
+            if (pagina == null) {
+                pagina = 1
+            }
+            let url = "Gestor_tbl_Fondos.php"
+            let formaData = new FormData()
+            formaData.append('campo', input)
+            formaData.append('registros', num_registros)
+            formaData.append('pagina', pagina)
+            formaData.append('orderCol', orderCol)
+            formaData.append('orderType', orderType)
+
+            fetch(url, {
+                    method: "POST",
+                    body: formaData
+                }).then(response => response.json())
+                .then(data => {
+                    content.innerHTML = data.data
+                    document.getElementById("lbl-total").innerHTML = 'Mostrando ' + data.totalFiltro +
+                        ' de ' + data.totalRegistros + ' registros'
+                    document.getElementById("nav-paginacion").innerHTML = data.paginacion
+                }).catch(err => console.log(err))
+        }
+
+        function nextPage(pagina){
+            document.getElementById('pagina').value = pagina
+            getData()
+        }
+
+        let columns = document.getElementsByClassName("sort")
+        let tamanio = columns.length
+        for(let i = 0; i < tamanio; i++){
+            columns[i].addEventListener("click", ordenar)
+        }
+
+        function ordenar(e){
+            let elemento = e.target
+
+            document.getElementById('orderCol').value = elemento.cellIndex
+
+            if(elemento.classList.contains("asc")){
+                document.getElementById("orderType").value = "asc"
+                elemento.classList.remove("asc")
+                elemento.classList.add("desc")
+            } else {
+                document.getElementById("orderType").value = "desc"
+                elemento.classList.remove("desc")
+                elemento.classList.add("asc")
+            }
+
+            getData()
+        }
+
+    </script>
+
+    <!-- Bootstrap core JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
                     <?php } ?>
                     <div class="panel-body" id="formularioregistros">
                         <form name="formulario" id="formulario" action="Insert_Fondo.php" method="POST">
---
+++ Sistema/Fondos/Gestor_tbl_Fondos.php
@@ -0,0 +1,154 @@
+<?php
+/*
+* Script: Cargar datos de lado del servidor con PHP y MySQL
+* Autor: Marco Robles
+* Team: Códigos de Programación
+*/
+session_start();     
+ $usuario=$_SESSION['user'];
+ $ID_Rol=$_SESSION['ID_Rol'];
+
+require '../../conexion_BD.php';
+
+/* Un arreglo de las columnas a mostrar en la tabla */
+$columns = ['ID_de_Fondo', 'nombre_T_Fondo', 'Nombre_del_Objeto', 'Cantidad_Rec', 'Valor_monetario','Nombre_del_proyecto','Nombre_D','Fecha_de_adquisicion_F'];
+
+/* Nombre de la tabla */
+$table = "tbl_fondos";
+
+$id = 'ID_de_Fondo';
+ 
+$campo = isset($_POST['campo']) ? $conexion->real_escape_string($_POST['campo']) : null;
+
+
+/* Filtrado */
+$where = '';
+
+if ($campo != null) {
+    $where = "WHERE (";
+
+    $cont = count($columns);
+    for ($i = 0; $i < $cont; $i++) {
+        $where .= $columns[$i] . " LIKE '%" . $campo . "%' OR ";
+    }
+    $where = substr_replace($where, "", -3);
+    $where .= ")";
+}
+
+/* Limit */
+$limit = isset($_POST['registros']) ? $conexion->real_escape_string($_POST['registros']) : 10;
+$pagina = isset($_POST['pagina']) ? $conexion->real_escape_string($_POST['pagina']) : 0;
+
+if (!$pagina) {
+    $inicio = 0;
+    $pagina = 1;
+} else {
+    $inicio = ($pagina - 1) * $limit;
+}
+
+$sLimit = "LIMIT $inicio , $limit";
+
+/**
+ * Ordenamiento
+ */
+
+ $sOrder = "";
+ if(isset($_POST['orderCol'])){
+    $orderCol = $_POST['orderCol'];
+    $oderType = isset($_POST['orderType']) ? $_POST['orderType'] : 'asc';
+    
+    $sOrder = "ORDER BY ". $columns[intval($orderCol)] . ' ' . $oderType;
+ }
+
+
+/* Consulta */
+
+$sql="SELECT f.ID_de_fondo,tf.nombre_T_Fondo,f.Nombre_del_Objeto,f.Cantidad_Rec,f.Valor_monetario, p.Nombre_del_proyecto, d.Nombre_D, f.Fecha_de_adquisicion_F
+FROM tbl_fondos f 
+JOIN tbl_tipos_de_fondos tf ON f.ID_Tipo_Fondo = tf.ID_Tipo_Fondo
+JOIN tbl_donantes d ON f.ID_Donante = d.ID_Donante
+JOIN tbl_proyectos p ON f.ID_Proyecto = p.ID_proyecto
+WHERE f.ID_de_fondo LIKE '%{$campo}%' OR tf.nombre_T_Fondo LIKE '%{$campo}%' OR f.Nombre_del_Objeto LIKE '%{$campo}%' OR f.Cantidad_Rec LIKE '%{$campo}%' OR f.Valor_monetario LIKE '%{$campo}%'
+OR p.Nombre_del_proyecto LIKE '%{$campo}%' OR d.Nombre_D LIKE '%{$campo}%' OR f.Fecha_de_adquisicion_F LIKE '%{$campo}%'
+ORDER BY {$columns[$orderCol]} {$oderType}
+LIMIT {$inicio}, {$limit}";
+$resultado = $conexion->query($sql);
+$num_rows = $resultado->num_rows;
+
+/* Consulta para total de registro filtrados */
+$sqlFiltro = "SELECT FOUND_ROWS()";
+$resFiltro = $conexion->query($sqlFiltro);
+$row_filtro = $resFiltro->fetch_array();
+$totalFiltro = $row_filtro[0];
+
+/* Consulta para total de registro filtrados */
+$sqlTotal = "SELECT count($id) FROM $table ";
+$resTotal = $conexion->query($sqlTotal);
+$row_total = $resTotal->fetch_array();
+$totalRegistros = $row_total[0];
+
+/* Mostrado resultados */
+$output = [];
+$output['totalRegistros'] = $totalRegistros;
+$output['totalFiltro'] = $totalFiltro;
+$output['data'] = '';
+$output['paginacion'] = '';
+
+if ($num_rows > 0) {
+    while ($row = $resultado->fetch_assoc()) {
+        $output['data'] .= '<tr>';
+        $output['data'] .= '<td>' . $row['ID_de_fondo'] . '</td>';
+        $output['data'] .= '<td>' . $row['nombre_T_Fondo'] . '</td>';
+        $output['data'] .= '<td>' . $row['Nombre_del_Objeto'] . '</td>';
+        $output['data'] .= '<td>' . $row['Cantidad_Rec'] . '</td>'; 
+        $output['data'] .= '<td>' . $row['Valor_monetario'] . '</td>';
+        $output['data'] .= '<td>' . $row['Nombre_del_proyecto'] . '</td>';
+        $output['data'] .= '<td>' . $row['Nombre_D'] . '</td>';
+        $output['data'] .= '<td>' . $row['Fecha_de_adquisicion_F'] . '</td>';
+         $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Actualizacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
+if ($datos=$sql->fetch_object()) {
+        $output['data'] .= '<td><a class="boton-editar" href="Update_Fondo.php?ID_de_fondo=' . $row['ID_de_fondo'] . '"><i class="zmdi zmdi-edit"></i></a></td>';
+}
+$sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Eliminacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
+if ($datos=$sql->fetch_object()) { 
+        $output['data'] .= '<td><a class="boton-eliminar" href="Delete_Fondo.php?ID_de_fondo=' . $row['ID_de_fondo'] . '"><i class="zmdi zmdi-delete"></i></a></td>';
+}
+$output['data'] .= '</tr>';
+    }
+} else {
+    $output['data'] .= '<tr>';
+    $output['data'] .= '<td colspan="7">Sin resultados</td>';
+    $output['data'] .= '</tr>';
+}
+
+if ($output['totalRegistros'] > 0) {
+    $totalPaginas = ceil($output['totalRegistros'] / $limit);
+
+    $output['paginacion'] .= '<nav>';
+    $output['paginacion'] .= '<ul class="pagination">';
+
+    $numeroInicio = 1;
+
+    if(($pagina - 4) > 1){
+        $numeroInicio = $pagina - 4;
+    }
+
+    $numeroFin = $numeroInicio + 9;
+
+    if($numeroFin > $totalPaginas){
+        $numeroFin = $totalPaginas;
+    }
+
+    for ($i = $numeroInicio; $i <= $numeroFin; $i++) {
+        if ($pagina == $i) {
+            $output['paginacion'] .= '<li class="page-item active"><a class="page-link" href="#">' . $i . '</a></li>';
+        } else {
+            $output['paginacion'] .= '<li class="page-item"><a class="page-link" href="#" onclick="nextPage(' . $i . ')">' . $i . '</a></li>';
+        }
+    }
+
+    $output['paginacion'] .= '</ul>';
+    $output['paginacion'] .= '</nav>';
+}
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
