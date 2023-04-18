e8d6e8c3366c00cf255ccb1ee9b9648801d815f0
FernandoGalo <fernando.galo@unah.hn>
2023-04-18 00:44:30 -0600

    correccion

--- Sistema/Fondos/FondosAdm.php
+++ Sistema/Fondos/FondosAdm.php
@@ -39,9 +39,6 @@ $R_F_Vencida= date("Y-m-j",strtotime($R_Fecha_actual."+ ".$diasV." days")); /*le
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="../../css/main.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script type="text/javascript">
     function confirmar(){
       return confirm('¿Está Seguro?, se eliminará el Fondo');
