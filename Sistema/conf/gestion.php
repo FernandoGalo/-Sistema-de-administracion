37c30afa35cf4a1695efe622e5d3392bbb2249e1
ElidaAlvarado <elida.alvarado@unah.hn>
2023-04-18 21:11:43 -0600

    Estilo A la Gestion de Usuario

--- Sistema/conf/gestion.php
+++ Sistema/conf/gestion.php
@@ -1,5 +1,4 @@
-<style>
-</style>
+
 <?php
 require '../../conexion_BD.php';
 /*esta variable impide que se pueda entrar al sistema principal si no se entra por login (crea un usuario global) */
@@ -11,16 +10,34 @@ $ID_usuario = $_SESSION['ID_User'];
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>Inicio</title>
+	<title>Perfil</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="../../css/main.css">
+    <!-- Incluya el archivo CSS de Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <style>
+		/* Personaliza el ancho de los campos de entrada */
+		.form-control-sm {
+			width: 500px;
+		}
+
+    .container-fluid h1{
+    text-align: center;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    color: black;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    
+
+	}
+	</style>
   
 <script>
 function opencontra() {
   // Definir las dimensiones de la ventana emergente
-  var width = 400;
-  var height = 400;
+  var width = 500;
+  var height = 500;
 
   // Obtener un identificador único para la ventana emergente
   var id = Date.now();
@@ -47,62 +64,73 @@ function opencontra() {
 				</li>
 			</ul>
 		</nav>
+
 		<!-- Muestra el contenido de la pagina -->
 		<div class="container-fluid">
         <div class="row">
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                      <div class="contenedor">
-                          <h1 class="box-title">Gestionar mi perfil</h1>
-                          </div>
+                     
+                        <div class="container-fluid" class="full-box cover dashboard-sideBar" style="overflow-y: auto;">
+                              <h1>Gestion de Usuario</h1>
+                              <hr style="margin-bottom: 20px;">
                         </div>
-                        <br>
+
+                      <?php 
+                          $sql="SELECT * FROM tbl_ms_usuario where ID_Usuario='".$ID_usuario."'";
+                          $resultado=mysqli_query($conexion,$sql);
+
+                          $fila=mysqli_fetch_assoc($resultado);
+
+                          $Nombre_Usuario=$fila['Nombre_Usuario'];
+                          $Usuario=$fila['Usuario'];
+                          $Contraseña=$fila['Contraseña'];
+                          $Correo_electronico=$fila['Correo_electronico'];
+                          mysqli_close($conexion);
+                          ?>
+
+      <div class="container">
+    
+        <div class="row">
+            <!-- Columna de datos personales -->
+            <div class="col-lg-6">
+                <h2 class="font-weight-bold pb-2 border-bottom">Datos Personales</h2>
+                <form>
+                    <div class="form-group">
+                        <label for="nombre">Nombre: </label>
+                        <input class="form-control" type="text" id="N_U_Imput" name="N_U_Imput" value="<?php echo $Nombre_Usuario ?>" readonly require>
                     </div>
-                    <?php 
-                    
-            $sql="SELECT * FROM tbl_ms_usuario where ID_Usuario='".$ID_usuario."'";
-            $resultado=mysqli_query($conexion,$sql);
-
-            $fila=mysqli_fetch_assoc($resultado);
-
-            $Nombre_Usuario=$fila['Nombre_Usuario'];
-            $Usuario=$fila['Usuario'];
-            $Contraseña=$fila['Contraseña'];
-            $Correo_electronico=$fila['Correo_electronico'];
-            mysqli_close($conexion);
-            ?>
-                    <!-- /.box-header -->
-                    <!-- centro -->
-                    <div class="panel-body">
-    <div class="izquierda">             
-    <h3>Informacion personal</h3>
-    <h3></h3>
-    <h3>Nombre del usuario</h3>
-    <input type="text" id="N_U_Imput" name="N_U_Imput" value="<?php echo $Nombre_Usuario ?>" readonly require>
-    <h3>usuario</h3>
-    <input type="text" id="U_Imput" name="U_Imput" value="<?php echo $Usuario ?>" readonly require>
-    <h3>Correo electronico</h3>
-    <input type="text" id="C_E_Imput" name="C_E_Imput" value="<?php echo $Correo_electronico ?>" readonly require>
-    <h1></h1>
-    </div>
-    <div class="derecha"> 
-    <h3></h3>
-    <h3>Seguridad</h3>
-    <h3></h3>
-    <h3>contraseña</h3>
-    <input type="password" value="<?php echo $Contraseña ?>">
-    <h3></h3>
-    <button onclick="opencontra()">Cambiar contraseña</button>
-    </div>
-</div>
-
+                    <div class="form-group">
+                        <label for="apellido">Usuario: </label>
+                        <input class="form-control" type="text" id="U_Imput" name="U_Imput" value="<?php echo $Usuario ?>" readonly require>
+                    </div>
+                    <div class="form-group">
+                        <label for="telefono">Correo Electronico: </label>
+                        <input class="form-control" type="text" id="C_E_Imput" name="C_E_Imput" value="<?php echo $Correo_electronico ?>" readonly require>
+                    </div>
+                   <!-- <button type="submit" class="btn btn-primary">Guardar Cambios</button>-->
+                </form>
+            </div>
+            <!-- Columna de gestión de contraseña -->
+            <div class="col-lg-6">
+                <h2 class="font-weight-bold pb-2 border-bottom">Gestión de Contraseña</h2>
+                <form>
+                    <div class="form-group">
+                        <label for="contraseñaActual">Contraseña: </label>
+                        <input class="form-control" type="password" value="<?php echo $Contraseña ?>" readonly>
+                    </div>
+                    <button style="margin-bottom: 20px;" type="submit" class="btn btn-primary" onclick="opencontra()">Cambiar contraseña</button>
+                </form>                    
+                        
+                
+                        </div>
                     </div>
                     <!--Fin centro -->
                   </div><!-- /.box -->
               </div><!-- /.col -->
           </div><!-- /.row -->
-		</div>
+		
 	</section>
 
 
