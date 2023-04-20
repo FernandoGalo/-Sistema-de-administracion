8a466a6537b12816c528266c0616ffcab5b8531a
ElidaAlvarado <elida.alvarado@unah.hn>
2023-04-20 00:12:24 -0600

    Diseño Backup

--- Sistema/seguridad/Backups_BD.php
+++ Sistema/seguridad/Backups_BD.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>Inicio</title>
+	<title>Backups</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="../../css/main.css">
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
 
 <!-- Funcion Confirmar para eliminar-->
   <script type="text/javascript">
     function confirmar(){
-      return confirm('¿Está Seguro?, se eliminará la copia de seguridad del sistema');
+      return confirm('¿Está Seguro?, De eliminar la copia de seguridad del sistema');
     }
   </script>
 
@@ -88,6 +92,7 @@ if(isset($_POST['crear_copia_btn'])) {
 
 ?>
 </head>
+
 <body>
 <?php
 session_start();     
@@ -105,73 +110,104 @@ $ID_Rol=$_SESSION['ID_Rol'];
 				</li>
 			</ul>
 		</nav>
-    <div class="contenedor">
-    <h1>Backups y Restauraciones</h1>
-    </div>
-    <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_consultar=1 and ID_Rol=$ID_Rol and ID_Objeto=12");
-if ($datos=$sql->fetch_object()) { ?>
-<?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Insercion=1 and ID_Rol=$ID_Rol and ID_Objeto=12");
-if ($datos=$sql->fetch_object()) { ?>
-  <div class="izquierda">
-<form method="post" enctype="multipart/form-data">
-    <h3>Guardar una copia de seguridad</h3>
-    <h2></h2>
-    <button class="zmdi zmdi-cloud-download" type="submit" name="crear_copia_btn">Crear copia de seguridad</button>
-</form>
-</div>
-<?php } ?>
-<?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Actualizacion=1 and ID_Rol=$ID_Rol and ID_Objeto=12");
-if ($datos=$sql->fetch_object()) { ?>
-<div class="derecha">
-<h1>Restaurar una copia de seguridad</h1>
-  <form action="restore.php" method="post">
-    <select class="form-control" id="backup_file" name="backup_file">
-    <option value="">Seleccione un archivo de backup:</option>
-      <?php
-      // Ruta absoluta de la carpeta de backups
-      $backups_folder = "C:/xampp/htdocs/Sistema-administrativo-de-fondos-y-proyectos/Sistema/seguridad/Backups/";
 
-      // Obtener la lista de archivos de backup disponibles
-      $backup_files = glob($backups_folder . "*.sql");
+    <div class="container" style="width:850px">
+        <h1 style="text-align:center;" class="my-4">Administracion de Base de Datos</h1><hr>
 
-      // Mostrar la lista de archivos de backup en el formulario
-      foreach ($backup_files as $backup_file) {
-        echo "<option value=\"" . basename($backup_file) . "\">" . basename($backup_file) . "</option>";
-      }
-      ?>
-    </select>
-    <br><br>
-    <button class="zmdi zmdi-cloud-upload" type="submit" onclick='return confirmar1()'> Restaurar Backup</button>
-  </form>
-</div>
-<?php } ?>
-<?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Eliminacion=1 and ID_Rol=$ID_Rol and ID_Objeto=12");
-if ($datos=$sql->fetch_object()) { ?>
-<h1>Eliminar un archivo de copia de seguridad</h1>
-<form action="Eliminar_backup.php" method="post">
-<select class="form-control" id="backup_file" name="backup_file" style="max-width: 300px;">
-        <option value="">Seleccione un archivo para eliminar:</option>
-        <?php
-        // Ruta absoluta de la carpeta de backups
-        $backups_folder = "C:/xampp/htdocs/Sistema-administrativo-de-fondos-y-proyectos/Sistema/seguridad/Backups/";
+         <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_consultar=1 and ID_Rol=$ID_Rol and ID_Objeto=12");
+          if ($datos=$sql->fetch_object()) { ?>
+        <!-- Inicio permiso insertar Copia de Seguridad -->
+          <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Insercion=1 and ID_Rol=$ID_Rol and ID_Objeto=12");
+          if ($datos=$sql->fetch_object()) { ?>
+            <!-- Inicio formulario Copia de Seguridad -->
+            <div class="card mb-4">
+              <div class="card-header" style="background-color: #F6DDCC; font-family: Arial, sans-serif;">Copia de Seguridad</div>
+              <div class="card-body">
+                <form method="post" enctype="multipart/form-data">
+                <label>Preciona el boton si deseas realizar una Copia de Seguridad</label>
+                  <div class="form-group" style="margin-top:10px">
+                  <button class="btn btn-success" style="background-color: #28B463 ;" type="submit" name="crear_copia_btn">
+                    <i class="	fas fa-cloud-download-alt"></i> Crear copia de seguridad
+                  </button>
+                  </div>
+                </form>
+              </div>
+            </div><!-- Fin Copia de seguridad -->
+          <?php } ?> <!-- Fin Del permiso de insercion -->
 
-        // Obtener la lista de archivos de backup disponibles
-        $backup_files = glob($backups_folder . "*.sql");
+          <!-- Inicio permiso actualizar (restaurar) Copia de Seguridad -->
+          <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Actualizacion=1 and ID_Rol=$ID_Rol and ID_Objeto=12");
+          if ($datos=$sql->fetch_object()) { ?>
+          <!-- Inicio formulario restaurar Copia de Seguridad -->
+          <div class="card mb-4">
+            <div class="card-header" style="background-color: #F6DDCC; font-family: Arial, sans-serif;"> Restaurar una Copia de Seguridad</div>
+              <div class="card-body">
+                <form action="restore.php" method="post">
+                  <div class="form-group">
+                    <label>Seleccionar el archivo a restaurar</label>
+                    <select class="form-control" id="backup_file" name="backup_file">
+                      <option value="">Seleccione un archivo</option>
+                        <?php
+                        // Ruta absoluta de la carpeta de backups
+                        $backups_folder = "C:/xampp/htdocs/Sistema-administrativo-de-fondos-y-proyectos/Sistema/seguridad/Backups/";
 
-        // Mostrar la lista de archivos de backup en el formulario
-        foreach ($backup_files as $backup_file) {
-            echo "<option value=\"" . basename($backup_file) . "\">" . basename($backup_file) . "</option>";
-        }
-        ?>
-    </select>
-    <br><br>
-    <button class="zmdi zmdi-close" type="submit" onclick='return confirmar()'>Eliminar Backup</button>
-</form>
+                        // Obtener la lista de archivos de backup disponibles
+                        $backup_files = glob($backups_folder . "*.sql");
+
+                        // Mostrar la lista de archivos de backup en el formulario
+                        foreach ($backup_files as $backup_file) {
+                          echo "<option value=\"" . basename($backup_file) . "\">" . basename($backup_file) . "</option>";
+                        }
+                        ?>
+                      </select>
+                  </div>
+                  <button type="submit" style="background-color: #2E86C1" class="btn btn-primary" onclick='return confirmar1()'> <i class="fa fa-upload"> </i> Restaurar Copia de Seguridad </button>
+                  </form>
+                </div>
+          </div><!-- Fin restaurar Copia de seguridad -->
+          <?php } ?><!-- Fin Del permiso de actualizar -->
+
+            <!-- Inicio permiso eliminar Copia de Seguridad -->
+            <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Eliminacion=1 and ID_Rol=$ID_Rol and ID_Objeto=12");
+              if ($datos=$sql->fetch_object()) { ?>
+              <!-- Inicio formulario eliminar Copia de Seguridad -->
+              <div class="card mb-4">
+                <div class="card-header" style="background-color: #F6DDCC; font-family: Arial, sans-serif;">Eliminar una Copia de Seguridad</div>
+                <div class="card-body">
+                  <form action="Eliminar_backup.php" method="post">
+                    <div class="form-group">
+                      <label>Seleccione el archivo a eliminar</label>
+                      <select class="form-control" id="backup_file" name="backup_file" >
+    
+                            <option value="">Seleccione un Archivo </option>
+                            <?php
+                            // Ruta absoluta de la carpeta de backups
+                            $backups_folder = "C:/xampp/htdocs/Sistema-administrativo-de-fondos-y-proyectos/Sistema/seguridad/Backups/";
+
+                            // Obtener la lista de archivos de backup disponibles
+                            $backup_files = glob($backups_folder . "*.sql");
+
+                            // Mostrar la lista de archivos de backup en el formulario
+                            foreach ($backup_files as $backup_file) {
+                                echo "<option value=\"" . basename($backup_file) . "\">" . basename($backup_file) . "</option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <button class="btn btn-danger" style="background-color: #CB4335 ;" type="submit" onclick='return confirmar()'> <i class="fa fa-trash"> </i> Eliminar Copia de Seguridad</button>
+                  </form>
+                  </div>
+                </div><!-- Fin eliminar Copia de seguridad -->
+              <?php } ?><!-- Fin Del permiso eliminar -->    
+
+      </div>
+     </section>
   <?php } ?>
-  </section>
-  <?php } ?>
-  </body>
+  
   <script src="../../js/Buscador.js"></script>
   <script src="../../js/jquery-3.1.1.min.js"></script>
   <script src="../../js/events.js"></script>
 	<script src="../../js/main.js"></script>
+  
+  </body>
+</html>
