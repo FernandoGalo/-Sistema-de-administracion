a24a8c31349fdf5cfbc7c2edc2628961de08c612
FernandoGalo <fernando.galo@unah.hn>
2023-04-20 01:26:15 -0600

    subida temporal

---
+++ fpdf/ReporteAreasTrab.php
@@ -0,0 +1,117 @@
+<?php
+
+require('./fpdf.php');
+
+class PDF extends FPDF
+{
+
+   // Cabecera de página
+   function Header()
+   {
+      include '../conexion_BD.php';//llamamos a la conexion BD
+
+      $consulta_info = $conexion->query(" select * from tbl_datos_asociacion ");//traemos datos de la empresa desde BD
+      $dato_info = $consulta_info->fetch_object();
+      $this->Image('asociacion.jpg', 250, 5, 40); //logo de la empresa,moverDerecha,moverAbajo,tamañoIMG
+      $this->SetFont('Arial', 'B', 19); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
+      $this->Cell(95); // Movernos a la derecha
+      $this->SetTextColor(0, 0, 0); //color
+      //creamos una celda o fila
+      $this->Cell(110, 15, utf8_decode($dato_info -> nombre), 0, 1, 'C', 0); // AnchoCelda,AltoCelda,titulo,borde(1-0),saltoLinea(1-0),posicion(L-C-R),ColorFondo(1-0)
+      $this->Ln(3); // Salto de línea
+      $this->SetTextColor(103); //color
+
+      /* UBICACION */
+      $this->Cell(1);  // mover a la derecha
+      $this->SetFont('Arial', 'B', 10);
+      $this->Cell(96, 10, utf8_decode("Ubicación : " . $dato_info -> ubicacion), 0, 0, '', 0);
+      $this->Ln(5);
+
+      /* TELEFONO */
+      $this->Cell(1);  // mover a la derecha
+      $this->SetFont('Arial', 'B', 10);
+      $this->Cell(59, 10, utf8_decode("Teléfono : " . $dato_info -> telefono), 0, 0, '', 0);
+      $this->Ln(5);
+
+      /* CORREO */
+      $this->Cell(1);  // mover a la derecha
+      $this->SetFont('Arial', 'B', 10);
+      $this->Cell(85, 10, utf8_decode("Correo : " . $dato_info -> correo), 0, 0, '', 0);
+      $this->Ln(5); //SERIA EL ESPACIADO ENTRE ESTE CAMPO Y EL TITULO DEL REPORTE
+
+       // Obtener la fecha actual
+       $fecha_actual = date('d/m/Y h:m:s');
+      //  $fecha_actual = date('Y-m-d H:i:s');
+      //  $fecha_actual = date('Y-m-d H:i:s');
+      $fecha_actual = date('d/m/Y H:i:s');
+      
+
+       // Escribir la fecha en el reporte
+       $this->Cell(1);  // mover a la derecha
+       $this->Cell(85,10,'Fecha: '.$fecha_actual,0,0,'', 0);
+       $this->Ln(7); //SERIA EL ESPACIADO ENTRE ESTE CAMPO Y EL TITULO DEL REPORTE
+
+      /* SUCURSAL */
+      // $this->Cell(180);  // mover a la derecha
+      // $this->SetFont('Arial', 'B', 10);
+      // $this->Cell(85, 10, utf8_decode("Sucursal : "), 0, 0, '', 0);
+      // $this->Ln(10);
+
+      /* TITULO DE LA TABLA */
+      //color
+      $this->SetTextColor(0, 95, 189);
+      $this->Cell(100); // mover a la derecha
+      $this->SetFont('Arial', 'B', 15);
+      $this->Cell(100, 10, utf8_decode("REPORTE DE ÁREAS DE TRABAJO"), 0, 1, 'C', 0);
+      $this->Ln(7);
+
+      /* CAMPOS DE LA TABLA */
+      //color
+      $this->SetFillColor(255, 255, 255); //colorFondo
+      $this->SetTextColor(000, 000, 000); //colorTexto
+      $this->SetDrawColor(255, 255, 255); //colorBorde 163 163 163
+      $this->SetFont('Arial', 'B', 11);
+      $this->Cell(40, 10, utf8_decode('N°'), 1, 0, 'C', 1);
+      $this->Cell(65, 10, utf8_decode('ÁREA DE TRABAJO'), 1, 0, 'C', 1);
+      $this->Cell(140, 10, utf8_decode('DESCRIPCIÓN'), 1, 1, 'C', 1);
+   }
+
+   // Pie de página
+   function Footer()
+   {
+      $this->SetY(-15); // Posición: a 1,5 cm del final
+      $this->SetFont('Arial', 'I', 8); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
+      $this->Cell(0, 10, utf8_decode('Página ') . $this->PageNo() . '/{nb}', 0, 0, 'C'); //pie de pagina(numero de pagina)
+
+      $this->SetY(-15); // Posición: a 1,5 cm del final
+      $this->SetFont('Arial', 'I', 8); //tipo fuente, cursiva, tamañoTexto
+      $hoy = date('d/m/Y h:m:s');
+      $this->Cell(540, 10, utf8_decode($hoy), 0, 0, 'C'); // pie de pagina(fecha de pagina)
+   }
+}
+
+include '../conexion_BD.php';
+//require '../../funciones/CortarCadena.php';
+/* CONSULTA INFORMACION DEL HOSPEDAJE */
+//$consulta_info = $conexion->query(" select *from hotel ");
+//$dato_info = $consulta_info->fetch_object();
+
+$pdf = new PDF();
+$pdf->AddPage("landscape"); /* aqui entran dos para parametros (horientazion,tamaño)V->portrait H->landscape tamaño (A3.A4.A5.letter.legal) */
+$pdf->AliasNbPages(); //muestra la pagina / y total de paginas
+
+$i = 0;
+$pdf->SetFont('Arial', '', 12);
+$pdf->SetDrawColor(163, 163, 163); //colorBorde
+
+$consulta_reporte_alquiler = $conexion->query("SELECT * FROM tbl_area_trabajo");
+
+while ($datos_reporte = $consulta_reporte_alquiler->fetch_object()) {   
+      $i = $i + 1;
+      /* TABLA */
+      $pdf->Cell(40, 10, utf8_decode($i), 0, 0, 'C', 0);
+      $pdf->Cell(65, 10, utf8_decode($datos_reporte -> nombre_Area_Trabajo), 0, 0, 'C', 0);
+      $pdf->Cell(140, 10, utf8_decode($datos_reporte -> descripcion_A_Trabajo), 0, 1, 'C', 0);
+   }
+
+$pdf->Output('ReporteDecSAR.pdf', 'I');//nombreDescarga, Visor(I->visualizar - D->descargar)
