8c7c0a632db5de8809ed01a0faabe9e1ea6933f1
OscarQuinUnah <oscar.quin@unah.hn>
2023-04-23 14:37:59 -0600

    reportes fpdf con tabla parametros

--- fpdf/ReporteRoles.php
+++ fpdf/ReporteRoles.php
@@ -9,37 +9,56 @@ class PDF extends FPDF
    {
       include '../conexion_BD.php';//llamamos a la conexion BD
 
-      $consulta_info = $conexion->query(" select * from tbl_datos_asociacion ");//traemos datos de la empresa desde BD
+      $consulta_info = $conexion->query(" select Valor from tbl_ms_parametros WHERE ID_Parametro = 12");//traemos datos de la empresa desde BD
       $dato_info = $consulta_info->fetch_object();
       $this->Image('asociacion.jpg', 250, 5, 40); //logo de la empresa,moverDerecha,moverAbajo,tamañoIMG
       $this->SetFont('Arial', 'B', 19); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
       $this->Cell(95); // Movernos a la derecha
       $this->SetTextColor(0, 0, 0); //color
       //creamos una celda o fila
-      $this->Cell(110, 15, utf8_decode($dato_info -> nombre), 0, 1, 'C', 0); // AnchoCelda,AltoCelda,titulo,borde(1-0),saltoLinea(1-0),posicion(L-C-R),ColorFondo(1-0)
+      $this->Cell(110, 15, utf8_decode($dato_info -> Valor), 0, 1, 'C', 0); // AnchoCelda,AltoCelda,titulo,borde(1-0),saltoLinea(1-0),posicion(L-C-R),ColorFondo(1-0)
       $this->Ln(3); // Salto de línea
       $this->SetTextColor(103); //color
 
+
+      $consulta_info = $conexion->query(" select Valor from tbl_ms_parametros WHERE ID_Parametro = 14");//traemos datos de la empresa desde BD
+      $dato_info = $consulta_info->fetch_object();
       /* UBICACION */
       $this->Cell(1);  // mover a la derecha
       $this->SetFont('Arial', 'B', 10);
-      $this->Cell(96, 10, utf8_decode("Ubicación : " . $dato_info -> ubicacion), 0, 0, '', 0);
+      $this->Cell(96, 10, utf8_decode("Ubicación : " . $dato_info -> Valor), 0, 0, '', 0);
       $this->Ln(5);
 
+
+      $consulta_info = $conexion->query(" select Valor from tbl_ms_parametros WHERE ID_Parametro = 13");//traemos datos de la empresa desde BD
+      $dato_info = $consulta_info->fetch_object();
       /* TELEFONO */
       $this->Cell(1);  // mover a la derecha
       $this->SetFont('Arial', 'B', 10);
-      $this->Cell(59, 10, utf8_decode("Teléfono : " . $dato_info -> telefono), 0, 0, '', 0);
+      $this->Cell(59, 10, utf8_decode("Teléfono : " . $dato_info -> Valor), 0, 0, '', 0);
       $this->Ln(5);
 
+
+      $consulta_info = $conexion->query(" select Valor from tbl_ms_parametros WHERE ID_Parametro = 15");//traemos datos de la empresa desde BD
+      $dato_info = $consulta_info->fetch_object();
       /* CORREO */
       $this->Cell(1);  // mover a la derecha
       $this->SetFont('Arial', 'B', 10);
-      $this->Cell(85, 10, utf8_decode("Correo : " . $dato_info -> correo), 0, 0, '', 0);
-      $this->Ln(15); //SERIA EL ESPACIADO ENTRE ESTE CAMPO Y EL TITULO DEL REPORTE
-// Obtener la fecha actual
-//  $fecha_actual = date('Y-m-d H:i:s');
-//  $fecha_actual = date('Y-m-d H:i:s');
+      $this->Cell(85, 10, utf8_decode("Correo : " . $dato_info -> Valor), 0, 0, '', 0);
+      $this->Ln(5); //SERIA EL ESPACIADO ENTRE ESTE CAMPO Y EL TITULO DEL REPORTE
+
+       // Obtener la fecha actual
+       $fecha_actual = date('d/m/Y h:m:s');
+      //  $fecha_actual = date('Y-m-d H:i:s');
+      //  $fecha_actual = date('Y-m-d H:i:s');
+      $fecha_actual = date('d/m/Y H:i:s');
+      
+
+       // Escribir la fecha en el reporte
+       $this->Cell(1);  // mover a la derecha
+       $this->Cell(85,10,'Fecha: '.$fecha_actual,0,0,'', 0);
+       $this->Ln(7); //SERIA EL ESPACIADO ENTRE ESTE CAMPO Y EL TITULO DEL REPORTE
+
       /* SUCURSAL */
       // $this->Cell(180);  // mover a la derecha
       // $this->SetFont('Arial', 'B', 10);
